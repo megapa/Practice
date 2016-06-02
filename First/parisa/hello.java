f5dd47f855b35a202628378d3421b544ae1fa639
parisa <artimis17m@gmail.com>
2016-06-02 22:19:57 +0430

    add hello from parisa with some thing true

--- First/parisa/hello.java
+++ First/parisa/hello.java
@@ -1,5 +1,6 @@
 
-
-public static void main(String[] args){
-	System.out.println("parisa");
+class balad_nistamkhob{
+	public static void main(String[] args){
+		System.out.println("parisa");
+	}
 }
